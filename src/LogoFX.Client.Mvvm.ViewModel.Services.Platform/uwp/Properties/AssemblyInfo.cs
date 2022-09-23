7bca8df6a2f8d7a7b252e9eba897e951fbbae8ed
Gennady Verdel <godrose@gmail.com>
2022-09-23 18:14:54 +0300

    chore: bump version

--- src/LogoFX.Client.Mvvm.ViewModel.Services.Platform/uwp/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Services.Platform/uwp/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.6")]
-[assembly: AssemblyFileVersion("2.2.6")]
+[assembly: AssemblyVersion("2.2.7")]
+[assembly: AssemblyFileVersion("2.2.7")]
 [assembly: ComVisible(false)]
